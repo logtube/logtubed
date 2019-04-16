dfcd8258263035205d772143f5bda18f567c4cbb
Yanke Guo <guoyk@pagoda.com.cn>
2019-04-16 14:21:37 +0800

    scripts: add archive-linux.sh

---
+++ scripts/archive-linux.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+set -u
+
+cd $(dirname $0)/..
+
+rm -rf dist
+
+cp -rf systemd dist
+
+GO111MODULE=off CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags "-X main.Version=`date -u +%Y-%m-%d_%H-%M-%S`" -o dist/logtubed
+
+cp -rf scripts/_install.sh dist/install.sh
+
+tar czf logtubed-dist-linux-amd64.tar.gz dist
+
+rm -rf dist
