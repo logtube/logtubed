75877cb576ac72553d2c3cb9f70f1447fd154462
Yanke Guo <guoyk@pagoda.com.cn>
2019-04-16 15:15:02 +0800

    scripts: fix scripts

--- scripts/_install.sh
+++ scripts/_install.sh
@@ -6,16 +6,19 @@ set -u
 cd $(dirname $0)
 
 install -m 644 logtubed.service /lib/systemd/system/logtubed.service
-if ! -e /etc/logtubed.yml; then
+
+if ! [ -e /etc/logtubed.yml ]
+then
     install -m 644 logtubed.yml     /etc/logtubed.yml
 fi
 
-systemd daemon-reload
-systemd stop logtubed
+systemctl daemon-reload
+systemctl stop logtubed
 
 install -s -m 755 logtubed      /usr/bin/logtubed
 
-if ! id -u logtubed > /dev/null 2>&1; then
+if ! id -u logtubed > /dev/null 2>&1
+then
     useradd -d /var/lib/logtubed logtubed
 fi
 
