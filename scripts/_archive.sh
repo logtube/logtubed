e287269e889d6885a6899a1fc032fedc99664e4a
Guo Y.K <hi@guoyk.net>
2019-07-08 11:44:01 +0800

    fix build script

--- scripts/_archive.sh
+++ scripts/_archive.sh
@@ -9,7 +9,7 @@ rm -rf dist
 
 cp -rf systemd dist
 
-GO111MODULE=off go build -ldflags "-X main.Version=`date -u +%Y-%m-%d_%H-%M-%S`" -o dist/logtubed
+GO111MODULE=off CGO_ENABLED=0 go build -ldflags "-X main.Version=`date -u +%Y-%m-%d_%H-%M-%S`" -o dist/logtubed
 
 cp -rf scripts/_install.sh dist/install.sh
 
