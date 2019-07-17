832941235b5eaf61cb6aa1586370e2979bc7acf0
Guo Y.K <hi@guoyk.net>
2019-07-17 14:05:57 +0800

    contrib: fix _archive.sh

--- contrib/_archive.sh
+++ contrib/_archive.sh
@@ -12,8 +12,7 @@ mkdir -p dist
 cp -rf contrib/logtubed.yml contrib/logtubed.service dist/
 
 GO111MODULE=off CGO_ENABLED=0 go build -ldflags "-X main.Version=`date -u +%Y-%m-%d_%H-%M-%S`" -o dist/logtubed
-cd cmd/logtubemon && GO111MODULE=off CGO_ENABLED=0 go build -o ../../dist/logtubemon && cd ../..
-cd cmd/logtubedash && GO111MODULE=off CGO_ENABLED=0 go build -o ../../dist/logtubedash && cd ../..
+cd tools/logtubemon && GO111MODULE=off CGO_ENABLED=0 go build -o ../../dist/logtubemon && cd ../..
 
 cp -rf contrib/_install.sh dist/install.sh
 
