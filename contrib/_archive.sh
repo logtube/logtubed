d78d447d8d476363912e036bcbe12c3c55bd6533
Guo Y.K <hi@guoyk.net>
2019-07-22 14:14:05 +0800

    contrib: using go module with vendor for archiving

--- contrib/_archive.sh
+++ contrib/_archive.sh
@@ -11,8 +11,8 @@ mkdir -p dist
 
 cp -rf contrib/logtubed.yml contrib/logtubed.service dist/
 
-GO111MODULE=off CGO_ENABLED=0 go build -ldflags "-X main.Version=`date -u +%Y-%m-%d_%H-%M-%S`" -o dist/logtubed
-cd tools/logtubemon && GO111MODULE=off CGO_ENABLED=0 go build -o ../../dist/logtubemon && cd ../..
+GO111MODULE=on CGO_ENABLED=0 go build -mod vendor -ldflags "-X main.Version=`date -u +%Y-%m-%d_%H-%M-%S`" -o dist/logtubed
+cd tools/logtubemon && GO111MODULE=on CGO_ENABLED=0 go build -mod vendor -o ../../dist/logtubemon && cd ../..
 
 cp -rf contrib/_install.sh dist/install.sh
 
