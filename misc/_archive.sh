01b8021fd64b2d36fefe8d663a0a314c28830aa7
Guo Y.K <hi@guoyk.net>
2019-12-21 01:52:02 +0800

    add esmaint to archive script

--- misc/_archive.sh
+++ misc/_archive.sh
@@ -13,6 +13,7 @@ cp -rf misc/logtubed.yml misc/logtubed.service dist/
 
 GO111MODULE=on CGO_ENABLED=0 go build -mod vendor -ldflags "-X main.Version=`date -u +%Y-%m-%d_%H-%M-%S`" -o dist/logtubed
 cd tools/logtubemon && GO111MODULE=on CGO_ENABLED=0 go build -mod vendor -o ../../dist/logtubemon && cd ../..
+cd tools/esmaint && GO111MODULE=on CGO_ENABLED=0 go build -mod vendor -o ../../dist/esmaint && cd ../..
 
 cp -rf misc/_install.sh dist/install.sh
 
