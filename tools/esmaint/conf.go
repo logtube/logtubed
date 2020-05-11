93dde31ffc4fc4e35f6c60e0d983a952b21fd1f3
Guo Y.K <guoyk@landzero.net>
2020-05-11 13:02:51 +0800

    修复

--- tools/esmaint/conf.go
+++ tools/esmaint/conf.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"sort"
+	"strconv"
 	"strings"
 )
 
@@ -14,12 +15,33 @@ type Rule struct {
 	Delete int64 `yaml:"delete"`
 }
 
+func ParseRule(s string) (rule Rule, err error) {
+	splits := strings.Split(s, ":")
+	if len(splits) != 4 {
+		err = errors.New("Rule 格式错误")
+		return
+	}
+	if rule.Warm, err = strconv.ParseInt(strings.TrimSpace(splits[0]), 10, 64); err != nil {
+		return
+	}
+	if rule.Move, err = strconv.ParseInt(strings.TrimSpace(splits[1]), 10, 64); err != nil {
+		return
+	}
+	if rule.Cold, err = strconv.ParseInt(strings.TrimSpace(splits[2]), 10, 64); err != nil {
+		return
+	}
+	if rule.Delete, err = strconv.ParseInt(strings.TrimSpace(splits[3]), 10, 64); err != nil {
+		return
+	}
+	return
+}
+
 type Conf struct {
-	URL             string          `yaml:"url" defaults:"http://127.0.0.1:9200"`
-	AlertDispatcher string          `yaml:"alert_dispatcher"`
-	TemplateDir     string          `yaml:"template_dir"`
-	Rules           map[string]Rule `yaml:"rules"`
-	Blacklist       []string        `yaml:"blacklist"`
+	URL             string            `yaml:"url" defaults:"http://127.0.0.1:9200"`
+	AlertDispatcher string            `yaml:"alert_dispatcher"`
+	TemplateDir     string            `yaml:"template_dir"`
+	Rules           map[string]string `yaml:"rules"`
+	Blacklist       []string          `yaml:"blacklist"`
 }
 
 func (c Conf) Validate() error {
@@ -32,27 +54,31 @@ func (c Conf) Validate() error {
 	if len(c.Rules) == 0 {
 		return errors.New("no rules provided")
 	}
-	for pfx, rule := range c.Rules {
-		if rule.Warm == 0 {
-			return fmt.Errorf("missing 'warm' in rule '%s'", pfx)
-		}
-		if rule.Move == 0 {
-			return fmt.Errorf("missing 'move' in rule '%s'", pfx)
-		}
-		if rule.Cold == 0 {
-			return fmt.Errorf("missing 'cold' in rule '%s'", pfx)
-		}
-		if rule.Delete == 0 {
-			return fmt.Errorf("missing 'delete' in rule '%s'", pfx)
-		}
-		if rule.Warm > rule.Move {
-			return fmt.Errorf("'warm' should be smaller than or equal to 'move' in rule '%s'", pfx)
-		}
-		if rule.Move >= rule.Cold {
-			return fmt.Errorf("'move' should be smaller than 'cold' in rule '%s'", pfx)
-		}
-		if rule.Cold >= rule.Delete {
-			return fmt.Errorf("'cold' should be smaller than 'delete' in rule '%s'", pfx)
+	for pfx, rawRule := range c.Rules {
+		if rule, err := ParseRule(rawRule); err != nil {
+			return err
+		} else {
+			if rule.Warm <= 0 {
+				return fmt.Errorf("missing 'warm' in rule '%s'", pfx)
+			}
+			if rule.Move <= 0 {
+				return fmt.Errorf("missing 'move' in rule '%s'", pfx)
+			}
+			if rule.Cold <= 0 {
+				return fmt.Errorf("missing 'cold' in rule '%s'", pfx)
+			}
+			if rule.Delete <= 0 {
+				return fmt.Errorf("missing 'delete' in rule '%s'", pfx)
+			}
+			if rule.Warm > rule.Move {
+				return fmt.Errorf("'warm' should be smaller than or equal to 'move' in rule '%s'", pfx)
+			}
+			if rule.Move >= rule.Cold {
+				return fmt.Errorf("'move' should be smaller than 'cold' in rule '%s'", pfx)
+			}
+			if rule.Cold >= rule.Delete {
+				return fmt.Errorf("'cold' should be smaller than 'delete' in rule '%s'", pfx)
+			}
 		}
 	}
 	return nil
@@ -78,7 +104,8 @@ func (c Conf) FindRule(index string) (Rule, bool) {
 
 	for _, prefix := range prefixes {
 		if strings.HasPrefix(index, prefix) {
-			return c.Rules[prefix], true
+			rule, _ := ParseRule(c.Rules[prefix])
+			return rule, true
 		}
 	}
 	return Rule{}, false
